0e2d30d4e5a6b19288fc372be1cc8191fb22b145
midfieldfieldhockey18 <Mcollins@lasell.edu>
2025-10-24 12:14:59 -0400

    Update http_requests.py

--- http_requests.py
+++ http_requests.py
@@ -1,1 +1,41 @@
-# Write your code here
+import requests
+
+class JSONPlaceholder:
+    def __init__(self, base_url):
+        self.base_url = base_url
+
+    def get_request(self):
+        response = requests.get(self.base_url)
+        return {
+            "status_code": response.status_code,
+            "headers": dict(response.headers),
+            "content": response.text[:500]
+        }
+
+    def post_request(self, data):
+        response = requests.post(self.base_url, json=data)
+        return {
+            "status_code": response.status_code,
+            "headers": dict(response.headers),
+            "content": response.text[:500]
+        }
+
+    def update_user(self, userId, title, body):
+        data = {
+            "title": title,
+            "body": body
+        }
+        url = f"{self.base_url}/{userId}"
+        response = requests.put(url, json=data)
+        return {
+            "status_code": response.status_code,
+            "headers": dict(response.headers),
+            "content": response.text[:500]
+        }
+
+    def delete_user(self, userId):
+        url = f"{self.base_url}/{userId}"
+        response = requests.delete(url)
+        return {
+            "status_code": response.status_code
+        }
